ea836692afb12c6c8e0e9c8d42b4e0971c7f3a28
Rahul Sharma <rahulgaur116@gmail.com>
2024-06-17 14:52:25 -0400

    changed folder structure

---
+++ client/src/js/database.js
@@ -0,0 +1,36 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => 
+  {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.add(content);
+    await tx.done;
+    // console.error('SUCCESS')
+  };
+  // console.error('putDb not implemented'); DO i need to name db as jatedb ?
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  return store.getAll();
+};
+// console.error('getDb not implemented');
+
+initdb();
